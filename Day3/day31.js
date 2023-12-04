6ceff6e0a538314b0e0ae5554ccbee9fc4e435ef
Mike Goldenberg <csrutil@tuta.io>
2023-12-04 22:16:45 +0100

    Advent Of Code Day 3 part 1

---
+++ Day3/day31.js
@@ -0,0 +1,61 @@
+import fs from "fs";
+
+export const myFile = fs.readFile("input.txt", "utf-8", (err, data) => {
+  if (err) throw err;
+  // console.log(data);
+  console.log(getPartOneSolution(data));
+});
+
+const sumArray = (arr) => arr.reduce((acc, a) => acc + a, 0);
+const getEngineSymbols = (lines) => {
+  const engineSymbols = new Map();
+  lines.forEach((line, lineNum) => {
+    for (let i = 0; i < line.length; i++) {
+      const symbol = line[i];
+      if (symbol !== "." && !Number.isInteger(parseInt(symbol, 10))) {
+        if (!engineSymbols.has(symbol)) {
+          engineSymbols.set(symbol, []);
+        }
+        engineSymbols.get(symbol)?.push({
+          location: { row: lineNum, column: i },
+          neighboringParts: [],
+        });
+      }
+    }
+  });
+  lines.forEach((line, lineNum) => {
+    const engineNumbers = [...line.matchAll(/\d+/g)].map((match) => ({
+      value: match[0],
+      valueAsNumber: parseInt(match[0], 10),
+      location: { row: lineNum, column: match.index },
+    }));
+    for (const engineNumber of engineNumbers) {
+      engineSymbols.forEach((symbolData) => {
+        symbolData.forEach((symbol) => {
+          if (
+            Math.abs(symbol.location.row - lineNum) <= 1 &&
+            symbol.location.column >= engineNumber.location.column - 1 &&
+            symbol.location.column <=
+              engineNumber.location.column + engineNumber.value.length
+          ) {
+            symbol.neighboringParts.push(engineNumber);
+          }
+        });
+      });
+    }
+  });
+  return engineSymbols;
+};
+export const getPartOneSolution = (input) => {
+  const lines = input.split("\n").filter(Boolean);
+  const engineSymbols = getEngineSymbols(lines);
+  const partNumberValues = [];
+  engineSymbols.forEach((symbolData) => {
+    symbolData.forEach((symbol) => {
+      symbol.neighboringParts.forEach(({ value }) => {
+        partNumberValues.push(parseInt(value, 10));
+      });
+    });
+  });
+  return sumArray(partNumberValues).toString();
+};
---
+++ Day3/day31.ts
@@ -0,0 +1,81 @@
+const sumArray = (arr: number[]) => arr.reduce((acc, a) => acc + a, 0);
+
+type Location = {
+  row: number;
+  column: number;
+};
+
+type EngineNumberPart = {
+  value: string;
+  valueAsNumber: number;
+  location: Location;
+};
+
+type EngineSymbolData = {
+  location: Location;
+  neighboringParts: Array<EngineNumberPart>;
+};
+
+type EngineSymbols = Map<string, Array<EngineSymbolData>>;
+
+const getEngineSymbols = (lines: string[]): EngineSymbols => {
+  const engineSymbols: EngineSymbols = new Map();
+  lines.forEach((line, lineNum) => {
+    for (let i = 0; i < line.length; i++) {
+      const symbol = line[i];
+      if (symbol !== "." && !Number.isInteger(parseInt(symbol, 10))) {
+        if (!engineSymbols.has(symbol)) {
+          engineSymbols.set(symbol, []);
+        }
+
+        engineSymbols.get(symbol)?.push({
+          location: { row: lineNum, column: i },
+          neighboringParts: [],
+        });
+      }
+    }
+  });
+
+  lines.forEach((line, lineNum) => {
+    const engineNumbers = [...line.matchAll(/\d+/g)].map<EngineNumberPart>(
+      (match) => ({
+        value: match[0],
+        valueAsNumber: parseInt(match[0], 10),
+        location: { row: lineNum, column: match.index! },
+      }),
+    );
+
+    for (const engineNumber of engineNumbers) {
+      engineSymbols.forEach((symbolData) => {
+        symbolData.forEach((symbol) => {
+          if (
+            Math.abs(symbol.location.row - lineNum) <= 1 &&
+            symbol.location.column >= engineNumber.location.column - 1 &&
+            symbol.location.column <=
+              engineNumber.location.column + engineNumber.value.length
+          ) {
+            symbol.neighboringParts.push(engineNumber);
+          }
+        });
+      });
+    }
+  });
+
+  return engineSymbols;
+};
+
+export const getPartOneSolution = (input: string): string => {
+  const lines = input.split("\n").filter(Boolean);
+  const engineSymbols = getEngineSymbols(lines);
+
+  const partNumberValues: number[] = [];
+  engineSymbols.forEach((symbolData) => {
+    symbolData.forEach((symbol) => {
+      symbol.neighboringParts.forEach(({ value }) => {
+        partNumberValues.push(parseInt(value, 10));
+      });
+    });
+  });
+
+  return sumArray(partNumberValues).toString();
+};
