4e5ea8e0bef2bc4e55eefbc7cd64faeaf70691a2
Mike Goldenberg <csrutil@tuta.io>
2023-12-06 13:11:09 +0100

    Advent Of Code Day 6 in python

---
+++ Day6/part1.py
@@ -0,0 +1,12 @@
+with open("input.txt", "r") as f:
+    lines = f.readlines()
+    times = list(map(int, lines[0].split()[1::]))
+    distances = list(map(int, lines[1].split()[1::]))
+    ways = [
+        sum(1 for hold in range(t) if hold * (t - hold) > d)
+        for t, d in zip(times, distances)
+    ]
+    r = 1
+    for w in ways:
+        r *= w
+    print(r)
