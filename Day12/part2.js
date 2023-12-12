9411f4e44db429a0cd465d6bf6697d4dbe20102e
Mike Goldenberg <csrutil@tuta.io>
2023-12-12 14:00:27 +0100

    Advent Of Code Day 12 part 2 wrong

---
+++ Day12/part2.js
@@ -0,0 +1,74 @@
+import Iter from "es-iter";
+import fs from "fs";
+
+const sumArray = (arr) => arr.reduce((acc, a) => acc + a, 0);
+export const getPartOneSolution = (input) => {
+  const lines = input.split("\n").filter(Boolean);
+  const records = lines.map((line) => {
+    const split = line.split(" ");
+    const springs = [...split[0]];
+    const damagedSpringGroupsLengths = split[1]
+      .split(",")
+      .map((d) => parseInt(d, 10));
+    return {
+      springs,
+      damagedSpringGroupsLengths,
+    };
+  });
+  const recordNumArrangements = [];
+  for (const record of records) {
+    const width = record.springs.length;
+    const totalOptions =
+      sumArray(record.damagedSpringGroupsLengths) +
+      record.damagedSpringGroupsLengths.length -
+      1;
+    const extraSquares = width - totalOptions;
+    const range = Array.from(
+      { length: record.damagedSpringGroupsLengths.length + extraSquares },
+      (_, i) => i,
+    );
+    const combinations = [
+      ...new Iter(range).combinations(record.damagedSpringGroupsLengths.length),
+    ];
+    const rows = combinations
+      .map((combiantion) => {
+        const row = [];
+        for (let i = 0; i < combiantion.length; i++) {
+          row.push(
+            ...Array.from(
+              { length: combiantion[i] - (combiantion[i - 1] ?? 0) },
+              () => ".",
+            ),
+          );
+          row.push(
+            ...Array.from(
+              { length: record.damagedSpringGroupsLengths[i] },
+              () => "#",
+            ),
+          );
+        }
+        row.push(...Array.from({ length: width - row.length }, () => "."));
+        for (let j = 0; j < width; j++) {
+          if (record.springs[j] === "?") {
+            continue;
+          }
+          if (row[j] !== record.springs[j]) {
+            return;
+          }
+        }
+        return row;
+      })
+      .filter(Boolean);
+    recordNumArrangements.push(rows.length);
+  }
+  return sumArray(recordNumArrangements).toString();
+};
+export const getPartTwoSolution = (input) => {
+  const lines = input.split("\n").filter(Boolean);
+  return lines.toString();
+};
+
+export const myPart2 = fs.readFile("input.txt", "utf8", (err, data) => {
+  if (err) throw err;
+  console.log(`Result for Part 2: ${getPartTwoSolution(data)}`);
+});
