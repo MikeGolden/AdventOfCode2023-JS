2f53b3a90e6808f87b5b55ff8c0e3aa373e0446d
Mike Goldenberg <csrutil@tuta.io>
2023-12-14 12:58:27 +0100

    Advent Of Code Day 14 part 1 wrong

---
+++ Day14/part1.js
@@ -0,0 +1,31 @@
+import fs from "fs";
+export const getPartOneSolution = (input) => {
+  const lines = input.split("\n").filter(Boolean);
+  const spaces = lines.map((line) => [...line]);
+  let iterate = true;
+  while (iterate) {
+    let didSlide = false;
+    for (let c = 0; c < lines[0].length; c++) {
+      for (let r = 1; r < lines.length; r++) {
+        if (spaces[r - 1][c] === "." && spaces[r][c] === "0") {
+          spaces[r - 1][c] = "0";
+          spaces[r][c] = ".";
+          didSlide = true;
+        }
+      }
+    }
+    if (!didSlide) {
+      iterate = false;
+    }
+  }
+  let sum = 0;
+  for (let r = 0; r < lines.length; r++) {
+    sum += (lines.length - r) * spaces[r].filter((s) => s === "0").length;
+  }
+  return sum.toString();
+};
+
+export const myPart1 = fs.readFile("input.txt", "utf8", (err, data) => {
+  if (err) throw err;
+  console.log(`The result for part 1: ${getPartOneSolution(data)}`);
+});
---
+++ Day14/part1.ts
@@ -0,0 +1,30 @@
+export const getPartOneSolution = (input: string): string => {
+  const lines = input.split("\n").filter(Boolean);
+
+  const spaces = lines.map((line) => [...line]);
+
+  let iterate = true;
+  while (iterate) {
+    let didSlide = false;
+    for (let c = 0; c < lines[0].length; c++) {
+      for (let r = 1; r < lines.length; r++) {
+        if (spaces[r - 1][c] === "." && spaces[r][c] === "0") {
+          spaces[r - 1][c] = "0";
+          spaces[r][c] = ".";
+          didSlide = true;
+        }
+      }
+    }
+
+    if (!didSlide) {
+      iterate = false;
+    }
+  }
+
+  let sum = 0;
+  for (let r = 0; r < lines.length; r++) {
+    sum += (lines.length - r) * spaces[r].filter((s) => s === "0").length;
+  }
+
+  return sum.toString();
+};
